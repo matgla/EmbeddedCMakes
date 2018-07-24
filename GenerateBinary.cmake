0b3b79446feb4719d3f3075b6c3476d98a5d0e6d
Mateusz Stadnik <matgla@live.com>
2018-07-24 22:43:15 +0200

    adding platform specific compiler options

--- GenerateBinary.cmake
+++ GenerateBinary.cmake
@@ -1,8 +1,6 @@
 function(generate_binary target_name)
-    add_custom_command(TARGET POST_BUILD ${target_name}
-        COMMAND ${arm_eabi_size} --format=berkeley "${target_name}.elf")
-
-    add_custom_target(${target_name}.hex DEPENDS ${target_name} COMMAND ${CMAKE_OBJCOPY} -Oihex ${target_name}.elf ${target_name}.hex)
-    file(TO_CMAKE_PATH "${CMAKE_OBJCOPY}" OBJCPY)
-    add_custom_target(${target_name}.bin DEPENDS ${target_name} COMMAND ${OBJCPY} -Obinary ${target_name}.elf ${target_name}.bin)
+    if (${vendor} STREQUAL "STM32")
+        include (stm32/GenerateStm32Binary)
+        generate_stm32_binary(${target_name})
+    endif ()
 endfunction()
