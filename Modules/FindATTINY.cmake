a99513a2ee2250bff0c2b431fd06839765462181
Mateusz Stadnik <matgla@live.com>
2019-09-04 19:02:59 +0200

    fixes for support avr_libstdcpp

--- Modules/FindATTINY.cmake
+++ Modules/FindATTINY.cmake
@@ -7,14 +7,14 @@ set(NO_STDCXX ON CACHE STRING "C++ standard library not exists" FORCE)
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake")
 
-add_compile_options(-mmcu=${mcu})
+message("Adding: ${CMAKE_CURRENT_SOURCE_DIR}/lib/avr_libstdcpp/avr_libstdcpp/include")
+add_compile_options("-mmcu=${mcu};-I${CMAKE_CURRENT_SOURCE_DIR}/lib/avr_libstdcpp/avr_libstdcpp/include;-fno-rtti;-fno-use-cxa-atexit;-fno-exceptions;-fno-threadsafe-statics")
 
 target_compile_options(avr INTERFACE
-
     -fdata-sections
     -ffunction-sections
     $<$<COMPILE_LANGUAGE:C>:-std=gnu99 -Wno-implicit-function-declaration>
-    $<$<COMPILE_LANGUAGE:CXX>:-std=c++1z -fno-rtti -fno-use-cxa-atexit -fno-exceptions -fno-threadsafe-statics -Wno-register>
+    $<$<COMPILE_LANGUAGE:CXX>:-std=c++1z -fno-rtti -fno-use-cxa-atexit -fno-exceptions -fno-threadsafe-statics -Wno-register >
     $<$<CONFIG:DEBUG>:-O0 -g>
     $<$<CONFIG:RELEASE>:-Os>
 )
