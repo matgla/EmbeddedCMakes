97baf6591a8cac974e094a9acb80ec9f020bb111
Mateusz Stadnik <matgla@live.com>
2019-12-02 22:06:20 +0100

    fixes for multiple executables

--- stm32/GenerateStm32Binary.cmake
+++ stm32/GenerateStm32Binary.cmake
@@ -1,10 +1,10 @@
 function(generate_stm32_binary target_name)
-    add_custom_command(OUTPUT size
-        COMMAND ${arm_eabi_size} --format=berkeley "${PROJECT_BINARY_DIR}/src/${target_name}.elf"
+    add_custom_command(OUTPUT ${target_name}_size
+        COMMAND ${arm_eabi_size} --format=berkeley "$<TARGET_FILE:${target_name}>"
         DEPENDS ${target_name})
 
-    add_custom_target(size_t ALL
-        DEPENDS size)
+    add_custom_target(${target_name}_size_t ALL
+        DEPENDS ${target_name}_size)
 
     add_custom_target(${target_name}.hex DEPENDS ${target_name} COMMAND ${CMAKE_OBJCOPY} -Oihex ${target_name}.elf ${target_name}.hex)
     file(TO_CMAKE_PATH "${arm_eabi_objcpy}" OBJCPY)
