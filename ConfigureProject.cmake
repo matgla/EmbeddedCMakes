154dbf87e1ad2358c5b907bee1da5344ec4d25b0
Mateusz Stadnik <matgla@live.com>
2020-05-05 18:52:30 +0200

    adding modules to cmake module path

--- ConfigureProject.cmake
+++ ConfigureProject.cmake
@@ -11,7 +11,9 @@ set(MCU ${unknown} CACHE STRING "Target MCU")
 set(MCU_FAMILY ${unknown} CACHE STRING "Target MCU family")
 
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_LIST_DIR}" CACHE STRING "CMAKE modules")
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_LIST_DIR}/Modules" CACHE STRING "CMAKE modules")
 
+message("AAAAAAAAAAAAAAAAAA: ${CMAKE_MODULE_PATH}")
 if (user_boards_path)
     set(board_file_glob_expression "${user_boards_path}/**/${BOARD}.cmake")
     message(STATUS "Searching board configuration: ${board_file_glob_expression}")
