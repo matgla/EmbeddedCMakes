c3227b048023f0d41829b4732c8e7ff963c925bc
Mateusz Stadnik <matgla@live.com>
2020-08-14 15:47:24 +0200

    fix for setting cmake_module_path to modules

--- ConfigureProject.cmake
+++ ConfigureProject.cmake
@@ -10,7 +10,8 @@ set(BOARD ${unknown} CACHE STRING "Board name")
 set(MCU ${unknown} CACHE STRING "Target MCU")
 set(MCU_FAMILY ${unknown} CACHE STRING "Target MCU family")
 
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_CURRENT_LIST_DIR}/Modules CACHE STRING "" FORCE)
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_CURRENT_LIST_DIR}/Modules PARENT_SCOPE)
+set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_CURRENT_LIST_DIR}/Modules)
 
 if (user_boards_path)
     set(board_file_glob_expression "${user_boards_path}/**/${BOARD}.cmake")
