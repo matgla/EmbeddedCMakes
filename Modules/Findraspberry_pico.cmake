00202616439a717ac3d6d85ffc546ae1a6ffb5a5
Mateusz Stadnik <matgla@live.com>
2021-02-14 10:03:54 +0100

    raspberry flags fixing

--- Modules/Findraspberry_pico.cmake
+++ Modules/Findraspberry_pico.cmake
@@ -37,21 +37,17 @@ set (CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${pico_sdk_SOURCE_DIR})
 
 #include (cmake/preload/toolchains/pico_arm_gcc)
 
-set (CMAKE_CXX_FLAGS_INIT ${CMAKE_CXX_FLAGS_INIT} -std=gnu++20)
+#set (CMAKE_CXX_FLAGS_INIT ${CMAKE_CXX_FLAGS_INIT} -std=gnu++20)
 
 set (hal_common_compilation_flags 
--mthumb -march=armv6-m -mcpu=cortex-m0plus
--fno-builtin -fdata-sections -ffunction-sections
+"-mthumb -march=armv6-m -mcpu=cortex-m0plus -fno-builtin -fdata-sections -ffunction-sections"
 )
 
 set (hal_cxx_compilation_flags ${hal_common_compilation_flags} 
     -fno-rtti -fno-exceptions
 )
 
-foreach(LANG IN ITEMS C CXX ASM) 
-    set (CMAKE_${LANG}_FLAGS ${hal_common_compilation_flags})
-    set (CMAKE_${LANG}_LINK_FLAGS "-Wl,--build-id=none")
-endforeach()
+set (CMAKE_CXX_FLAGS ${hal_common_compilation_flags})
 
 include (pico_sdk_init) 
 pico_sdk_init()
