858d003b401f84c751dc99c67b51529f0343c90a
Mateusz Stadnik <matgla@live.com>
2021-02-18 18:55:29 +0100

    added linker flags target for rpi pico

--- raspberry/GenerateRaspberryBinary.cmake
+++ raspberry/GenerateRaspberryBinary.cmake
@@ -1,10 +1,10 @@
 function(generate_raspberry_binary target_name)
-#    add_custom_command(OUTPUT ${target_name}_size
-#        COMMAND ${arm_eabi_size} --format=berkeley "$<TARGET_FILE:${target_name}>.elf"
-#        DEPENDS ${target_name})
+    add_custom_command(OUTPUT ${target_name}_size
+        COMMAND ${arm_eabi_size} --format=berkeley "$<TARGET_FILE:${target_name}>"
+        DEPENDS ${target_name})
 
-#    add_custom_target(${target_name}_size_t ALL
-#        DEPENDS ${target_name}_size)
+    add_custom_target(${target_name}_size_t ALL
+        DEPENDS ${target_name}_size)
 
     message (STATUS "Setting ${target_name} linker script to: ${linker_script}")
     pico_set_linker_script(${target_name} ${linker_script})
